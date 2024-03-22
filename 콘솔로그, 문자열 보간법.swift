5657e3826dc26d3da212cc913f91dfb8b009a89c
srkimdev <emirated3858@gmail.com>
2024-03-23 00:30:16 +0900

    콘솔로그, 문자열 보간법

---
+++ 콘솔로그, 문자열 보간법.swift
@@ -0,0 +1,22 @@
+import Swift
+
+let age: Int = 10
+
+"안녕하세요! 저는 \(age)살입니다"
+
+"안녕하세요! 저는 \(age + 5)살입니다"
+
+print("안녕하세요! 저는 \(age + 5)살입니다")
+
+print("\n######################\n")
+
+class Person {
+    var name: String = "yagom"
+    var age: Int = 10
+}
+
+let yagom: Person = Person()
+
+print(yagom)
+print("\n######################\n")
+dump(yagom)
