5a9da702d09722951461469bff89d97bdc1546a2
srkimdev <emirated3858@gmail.com>
2024-03-28 17:23:58 +0900

    구조체

---
+++ 구조체.swift
@@ -0,0 +1,75 @@
+import Swift
+
+// 구조체
+
+// struct 이름 {
+//     구현부
+// }
+
+struct Sample {
+    // 가변 프로퍼티(변수)
+    var mutableProperty: Int = 100
+
+    // 불변 프로퍼티
+    let immutableProperty: Int = 100
+
+    // 타입 프로퍼티
+    static var typeProperty: Int = 100
+
+    // 인스턴스 메서드(함수)
+    func instanceMethod() {
+        print("instance method")
+    }
+
+    // 타입 메서드
+    static func typeMethod() {
+        print("type method")
+    }
+
+}
+
+// 구조체 사용
+
+// 가변 인스턴스
+var mutable: Sample = Sample()
+mutable.mutableProperty = 200
+// mutable.immutableProperty = 200
+
+// 불면 인스턴스
+let immutable: Sample = Sample()
+// immutable.mutableProperty = 200
+// immutable.immutableProperty = 200
+
+// 타입 프로퍼티 및 메서드
+Sample.typeProperty = 300
+Sample.typeMethod() // type method
+
+// mutable.typeProperty = 400
+// mutable.typeMethod()
+// 인스턴스에서 타입 메서드나 프로퍼티를 사용할 수 없다.
+
+struct Student {
+    var name: String = "unknown"
+    var `class`: String = "Swift"
+
+    static func selfIntroduce() {
+        print("학생타입입니다")
+    }
+
+    func selfIntroduce() {
+        print("저는 \(self.class)반 \(name)입니다")
+    }
+}
+
+Student.selfIntroduce() // 학생타입입니다
+
+var yagom: Student = Student()
+yagom.name = "yagom"
+yagom.class = "스위프트"
+yagom.selfIntroduce() // 저는 스위프트반 yagom입니다
+
+let jina: Student = Student()
+// 불변 인스턴스이므로 프로퍼티 값 변경 불가
+// jina.name = "jina"
+
+jina.selfIntroduce() // 저는 Swift반 unknown입니다
